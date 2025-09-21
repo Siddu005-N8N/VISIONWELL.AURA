32bbffe04cc4495682dcc16d6531e976466b5288
Siddu005-N8N <srevanthkumar.ml@gmail.com>
2025-09-21 23:05:23 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ if (!rootElement) throw new Error('Failed to find the root element')
 
 const root = createRoot(rootElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(App, null)
+  )
 )
