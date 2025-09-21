48bda0f2d67f2c2863274114896ed42c59726f2d
Siddu005-N8N <srevanthkumar.ml@gmail.com>
2025-09-21 23:55:02 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
 import './index.css'
 
 const rootElement = document.getElementById("root")
@@ -8,7 +8,7 @@ if (!rootElement) throw new Error('Failed to find the root element')
 
 const root = createRoot(rootElement)
 root.render(
-  React.createElement(React.StrictMode, null,
-    React.createElement(App, null)
-  )
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
